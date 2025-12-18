6f539b47ceeeb9ace346a259ca8619acf0e70ce6
Anurag Verma <anuragverma4895@gmail.com>
2025-12-18 15:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.67%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=1;
+        long long high=x;
+        long long ans=0;
+        while(low<=high){
+            long long mid=(low+high)/2;
+            if(mid*mid<=x){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
