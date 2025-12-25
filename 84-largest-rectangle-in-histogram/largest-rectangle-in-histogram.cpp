e6dc5a58c1f3d1fa514c459b5622d4b8bfee7fcc
Anurag Verma <anuragverma4895@gmail.com>
2025-12-25 16:06:56 +0530

    Time: 26 ms (43.03%) | Memory: 85.8 MB (23.51%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int>nextSmaller(const vector<int>&v){
+        int n=v.size();
+        vector<int>ans(n,n);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0){
+                if(v[st.top()]>=v[i]){
+                    st.pop();
+                }else{
+                    ans[i]=st.top();
+                    break;
+                }
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int>prevSmaller(vector<int>&v){
+        int n=v.size();
+        vector<int>ans(n,-1);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(st.size()>0){
+                if(v[st.top()]>=v[i]){
+                    st.pop();
+                }else{
+                    ans[i]=st.top();
+                    break;
+                }
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        vector<int>next_smaller=nextSmaller(heights);
+        vector<int>prev_smaller=prevSmaller(heights);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int right_boun=next_smaller[i];
+            int left_boun=prev_smaller[i];
+            int curr=heights[i]*(right_boun-left_boun-1);
+            ans=max(ans,curr);
+        }
+        return ans;
+    }
+};
