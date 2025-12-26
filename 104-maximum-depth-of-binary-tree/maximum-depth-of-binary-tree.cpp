94ca71f0462e06479d0fb0718336897daa107ff0
Anurag Verma <anuragverma4895@gmail.com>
2025-12-27 00:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (12.46%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,25 +12,33 @@
  */
 class Solution {
 public:
+    // int maxDepth(TreeNode* root) {
+        // // Level order Traversal
+        // if(root==NULL) return 0;
+        // int ans = 0;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while (q.size()) {
+        //     int sz = q.size();
+        //     for (int i =0; i < sz; i++) {
+        //         TreeNode* curr = q.front();
+        //         q.pop();
+        //         if (curr->left != NULL) {
+        //             q.push(curr->left);
+        //         }
+        //         if (curr->right != NULL) {
+        //             q.push(curr->right);
+        //         }
+        //     }
+        //     ans++;
+        // }
+        // return ans;
+
+        // DFS
     int maxDepth(TreeNode* root) {
-        if(root==NULL) return 0;
-        int ans = 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (q.size()) {
-            int sz = q.size();
-            for (int i =0; i < sz; i++) {
-                TreeNode* curr = q.front();
-                q.pop();
-                if (curr->left != NULL) {
-                    q.push(curr->left);
-                }
-                if (curr->right != NULL) {
-                    q.push(curr->right);
-                }
-            }
-            ans++;
+        if(root==NULL){
+            return 0;
         }
-        return ans;
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
     }
 };
