6a35598323bc93209c4e849dc2a9d03aed7b734a
Anurag Verma <anuragverma4895@gmail.com>
2026-01-07 16:43:16 +0530

    Time: 3 ms (74.88%) | Memory: 78 MB (73.71%) - LeetSync

---
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long totalsum = 0;
+    long ans=LONG_MIN;
+    int total(TreeNode * root) {
+        if (root == NULL)
+            return 0;
+        long l=total(root->left);
+        long r=total(root->right);
+        long sub= root->val+ l+r;
+        ans=max(sub*(totalsum-sub),ans);
+        return sub;
+    }
+    int maxProduct(TreeNode* root) {
+        if(root==NULL) return 0;
+        totalsum=total(root);
+        total(root);
+        return ans%1000000007;
+    }
+};
