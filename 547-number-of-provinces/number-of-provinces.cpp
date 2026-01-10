8b498d0b9a054c3f692ab3664e5cc99bab9613bb
Anurag Verma <anuragverma4895@gmail.com>
2026-01-11 00:43:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (72.79%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
 
-    void bfs(vector<vector<int>>&adj, int node, vector<int>&vis){
-        queue<int>q;
-        q.push(node);
-        while(q.size()){
-            int curr=q.front();
-            q.pop();
-            if(vis[curr]==1) continue;
-            vis[curr]=1;
-            for(int neig=0;neig<adj.size();neig++){
-                if(adj[curr][neig]==1 && vis[neig]==0){
-                    q.push(neig);
-                }
-            }
+    // void bfs(vector<vector<int>>&adj, int node, vector<int>&vis){
+    //     queue<int>q;
+    //     q.push(node);
+    //     while(q.size()){
+    //         int curr=q.front();
+    //         q.pop();
+    //         if(vis[curr]==1) continue;
+    //         vis[curr]=1;
+    //         for(int neig=0;neig<adj.size();neig++){
+    //             if(adj[curr][neig]==1 && vis[neig]==0){
+    //                 q.push(neig);
+    //             }
+    //         }
 
+    //     }
+    // }
+
+    void dfs(vector<vector<int>>&adj, int node, vector<int>&vis){
+        vis[node]=1;
+        for(int i=0;i<adj.size();i++){
+            if(adj[node][i]==1 && vis[i]==0){
+                dfs(adj,i,vis);
+            }
         }
     }
 
@@ -24,7 +33,7 @@ public:
         int count=0;
         for(int i=0;i<n;i++){
             if(vis[i]==0){
-                bfs(isConnected, i, vis);
+                dfs(isConnected, i, vis);
                 count++;
             }
         }
