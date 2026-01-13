622a9d7ecf599ec21b748ff9748ba3d205c98477
Anurag Verma <anuragverma4895@gmail.com>
2026-01-13 23:43:15 +0530

    Time: 83 ms (8.78%) | Memory: 50.1 MB (9.09%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>dis(n,vector<int>(m,1e5));
+        queue<vector<int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({i,j,0});
+                    dis[i][j]=0;
+
+                }
+            }
+        }
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+        while(q.size()){
+            int cx=q.front()[0];
+            int cy=q.front()[1];
+            int level=q.front()[2];
+            q.pop();
+            for(int k=0;k<4;k++){
+                int nx=cx+dx[k];
+                int ny=cy+dy[k];
+                if(nx>=0 && nx<n && ny>=0 && ny<m && dis[nx][ny]==1e5){
+                    dis[nx][ny]=level+1;
+                    q.push({nx,ny,level+1});
+                }
+            }
+        }
+        return dis;
+    }
+};
