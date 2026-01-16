6d7cc80df51e30d8af7c2dee6d3704cb91b69b66
Anurag Verma <anuragverma4895@gmail.com>
2026-01-16 23:19:22 +0530

    Time: 230 ms (65.32%) | Memory: 10.5 MB (69.74%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
 
+    // bool dfs(vector<vector<char>>&board,string &word, int i,int j, int index,vector<vector<int>>&vis){
+    //     if(index==word.size()) return true;
+    //     int n=board.size();
+    //     int m=board[0].size();
+    //     if(i<0 || j<0||i>=n || j>=m) return false;
+    //     if(vis[i][j] || board[i][j] != word[index]) return false;
+    //     vis[i][j]=1;
+    //     bool ok=dfs(board, word, i+1, j, index+1, vis) ||
+    //         dfs(board, word, i-1, j, index+1, vis) ||
+    //         dfs(board, word, i, j+1, index+1, vis) ||
+    //         dfs(board, word, i, j-1, index+1, vis);
+    //     vis[i][j]=0;
+    //     return ok;
+    // }
+
     bool dfs(vector<vector<char>>&board,string &word, int i,int j, int index,vector<vector<int>>&vis){
         if(index==word.size()) return true;
         int n=board.size();
@@ -8,12 +23,18 @@ public:
         if(i<0 || j<0||i>=n || j>=m) return false;
         if(vis[i][j] || board[i][j] != word[index]) return false;
         vis[i][j]=1;
-        bool ok=dfs(board, word, i+1, j, index+1, vis) ||
-            dfs(board, word, i-1, j, index+1, vis) ||
-            dfs(board, word, i, j+1, index+1, vis) ||
-            dfs(board, word, i, j-1, index+1, vis);
+        int cx[]={1,-1,0,0};
+        int cy[]={0,0,-1,1};
+        for(int k=0;k<4;k++){
+            int ni=i+cx[k];
+            int nj=j+cy[k];
+            if(dfs(board, word,ni,nj,index+1, vis)){
+                vis[i][j]=0;
+                return true;
+            }
+        }
         vis[i][j]=0;
-        return ok;
+        return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
