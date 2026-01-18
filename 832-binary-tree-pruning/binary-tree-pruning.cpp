e8d563341372c45f13c7e0233e639cf5ba5b217c
Anurag Verma <anuragverma4895@gmail.com>
2026-01-19 00:21:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (43.50%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.cpp
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -11,24 +11,31 @@
  */
 class Solution {
 public:
-    TreeNode* pruneTree(TreeNode* root) {
+    // method 01
+    // TreeNode* pruneTree(TreeNode* root) {
+    //     if(root==NULL) return NULL;
+    //     root->left=pruneTree(root->left);
+    //     root->right=pruneTree(root->right);
+    //     if(root->left && root->left->val==0 && root->left->left==NULL && root->left->right==NULL){
+    //         root->left=NULL;
+    //     }
+    //     if(root->right && root->right->val==0 && root->right->left==NULL && root->right->right==NULL){
+    //         root->right=NULL;
+    //     }
+    //     if(root->val==0 && root->left==NULL && root->right==NULL)
+    //         return NULL;
+    //     return root;
+    // }
+
+
+    // method 02
+    TreeNode* pruneTree(TreeNode* root){
         if(root==NULL) return NULL;
-        // if(root->left && root->left->val==0){
-        //     root->left=NULL;
-        // }
-        // }else if(root->right && root->right->val==0){
-        //     root->right=NULL;
-        // }
         root->left=pruneTree(root->left);
         root->right=pruneTree(root->right);
-        if(root->left && root->left->val==0 && root->left->left==NULL && root->left->right==NULL){
-            root->left=NULL;
-        }
-        if(root->right && root->right->val==0 && root->right->left==NULL && root->right->right==NULL){
-            root->right=NULL;
-        }
-        if(root->val==0 && root->left==NULL && root->right==NULL)
+        if(root->val==0 && root->left==NULL && root->right==NULL){
             return NULL;
+        }
         return root;
     }
 };
