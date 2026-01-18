4ebb06da672fc4afb4abf58f9ee9a4bd786ca2c6
Anurag Verma <anuragverma4895@gmail.com>
2026-01-19 01:48:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (64.39%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -11,26 +11,45 @@
  */
 class Solution {
 public:
-    int countofnode(TreeNode* root){
-        if(root==NULL) return 0;
-        return 1+countofnode(root->left)+countofnode(root->right);
-    }
-    int sumofnode(TreeNode* root){
-        if(root==NULL) return 0;
-        return root->val+sumofnode(root->left)+sumofnode(root->right);
-    }
-    int count=0;
-    int averageOfSubtree(TreeNode* root) {
-        if(root==NULL) return 0;
-        int sum = sumofnode(root);
-        int tcount=countofnode(root);
-        int a=sum/tcount;
-        if(a==root->val){
-            count++;
-        }
-        averageOfSubtree(root->left);
-        averageOfSubtree(root->right);
-        return count;
 
+    // // method 01, TC= O(n^2)
+    // int countofnode(TreeNode* root){
+    //     if(root==NULL) return 0;
+    //     return 1+countofnode(root->left)+countofnode(root->right);
+    // }
+    // int sumofnode(TreeNode* root){
+    //     if(root==NULL) return 0;
+    //     return root->val+sumofnode(root->left)+sumofnode(root->right);
+    // }
+    // int count=0;
+    // int averageOfSubtree(TreeNode* root) {
+    //     if(root==NULL) return 0;
+    //     int sum = sumofnode(root);
+    //     int tcount=countofnode(root);
+    //     int a=sum/tcount;
+    //     if(a==root->val){
+    //         count++;
+    //     }
+    //     averageOfSubtree(root->left);
+    //     averageOfSubtree(root->right);
+    //     return count;
+    // }
+
+    // method 02, TC=O(n)
+    int ans=0;
+    pair<int,int>dfs(TreeNode* root){
+        if(root==NULL) return {0,0};
+        auto l=dfs(root->left);
+        auto r=dfs(root->right);
+        int sum=root->val+l.first+r.first;
+        int count=1+l.second+r.second;
+        if(sum/count==root->val){
+            ans++;
+        }
+        return {sum,count};
+    }
+    int averageOfSubtree(TreeNode* root){
+        dfs(root);
+        return ans;
     }
 };
