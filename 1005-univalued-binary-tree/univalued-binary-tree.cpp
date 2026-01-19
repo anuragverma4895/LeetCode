ed296fcb8d577f09b6262fc90b6d20f91a68ea3c
Anurag Verma <anuragverma4895@gmail.com>
2026-01-20 01:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (90.75%) - LeetSync

--- 1005-univalued-binary-tree/univalued-binary-tree.cpp
+++ 1005-univalued-binary-tree/univalued-binary-tree.cpp
@@ -11,26 +11,39 @@
  */
 class Solution {
 public:
-    bool isUnivalTree(TreeNode* root) {
-        vector<int>ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(q.size()){
-            TreeNode* curr=q.front();
-            q.pop();
-            ans.push_back(curr->val);
-            if(curr->left){
-                q.push(curr->left);
-            }
-            if(curr->right){
-                q.push(curr->right);
-            }
+    // BFS
+    // bool isUnivalTree(TreeNode* root) {
+    //     vector<int>ans;
+    //     queue<TreeNode*>q;
+    //     q.push(root);
+    //     while(q.size()){
+    //         TreeNode* curr=q.front();
+    //         q.pop();
+    //         ans.push_back(curr->val);
+    //         if(curr->left){
+    //             q.push(curr->left);
+    //         }
+    //         if(curr->right){
+    //             q.push(curr->right);
+    //         }
+    //     }
+    //     for(int i=1;i<ans.size();i++){
+    //         if(ans[i-1]!=ans[i]){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    // DFS
+    bool dfs(TreeNode* root, int val){
+        if(root==NULL) return true;
+        if(root->val!=val){
+            return false;
         }
-        for(int i=1;i<ans.size();i++){
-            if(ans[i-1]!=ans[i]){
-                return false;
-            }
-        }
-        return true;
+        return dfs(root->left,val) && dfs(root->right,val);
+    }
+    bool isUnivalTree(TreeNode* root){
+        return dfs(root,root->val);
     }
 };
