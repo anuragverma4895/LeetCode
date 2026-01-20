cf06e1dd6eb84e444f8ae2f70a408d8b90772f28
Anurag Verma <anuragverma4895@gmail.com>
2026-01-20 22:20:47 +0530

    Time: 62 ms (9.98%) | Memory: 50.3 MB (8.85%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
 
-    // this code is calculating the distance
+    // this code is calculating the distance by using bfs beacuse bfs 
+    // is always suitable for finding shortest path
     // important
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
         vector<vector<int>>dis(n,vector<int>(m,1e5));
-        queue<vector<int>>q;
+        queue<vector<int>>q; // i,j,dist  --> (store)
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
@@ -20,9 +21,9 @@ public:
         int dx[]={-1,1,0,0};
         int dy[]={0,0,-1,1};
         while(q.size()){
-            int cx=q.front()[0];
-            int cy=q.front()[1];
-            int level=q.front()[2];
+            int cx=q.front()[0]; // element in q at 0 index
+            int cy=q.front()[1]; // element in q at 1 index
+            int level=q.front()[2]; // element in q at 2 index
             q.pop();
             for(int k=0;k<4;k++){
                 int nx=cx+dx[k];
