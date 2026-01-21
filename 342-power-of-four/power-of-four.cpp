ecbba1298808a1de6e89cd04675da13414c9ab32
Anurag Verma <anuragverma4895@gmail.com>
2026-01-21 11:10:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.18%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        // int temp=pow(4,15);
+        // return n>0 && temp%n==0 && (n & 0x55555555);
+
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555);
+    }
+};
