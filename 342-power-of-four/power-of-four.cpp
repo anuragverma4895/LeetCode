bbe574c2c6119075c1a6efa6c541afe4d048132f
Anurag Verma <anuragverma4895@gmail.com>
2026-01-21 11:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (48.06%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -4,6 +4,6 @@ public:
         // int temp=pow(4,15);
         // return n>0 && temp%n==0 && (n & 0x55555555);
 
-        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555);
+        return n > 0 && (n & (n - 1)) == 0 && ((n & 0x55555555)!=0);
     }
 };
