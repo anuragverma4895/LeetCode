8af744db12737486036dab0494a7792a04f95bc5
Anurag Verma <anuragverma4895@gmail.com>
2026-02-07 00:19:11 +0530

    Time: 4 ms (52.24%) | Memory: 17.2 MB (62.78%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        // brute force
+        // int n=matrix.size();
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         ans.push_back(matrix[i][j]);
+        //     }
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans[k-1];
+
+
         int n=matrix.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans.push_back(matrix[i][j]);
+        int lo=matrix[0][0],hi=matrix[n-1][n-1];
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            int count=0;
+            int j=n-1;
+            for(int i=0;i<n;i++){
+                while(j>=0 && matrix[i][j]>mid){
+                    j--;
+                }
+                count+=(j+1);
+            }
+            if(count<k){
+                lo=mid+1;
+            }else{
+                hi=mid;
             }
         }
-        sort(ans.begin(),ans.end());
-        return ans[k-1];
-
+        return lo;
     }
 };
