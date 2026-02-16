ff53bea6e40ac5d8bebbf417a32011a55043e788
Anurag Verma <anuragverma4895@gmail.com>
2026-02-16 20:45:41 +0530

    Time: 6 ms (40.28%) | Memory: 28.5 MB (58.96%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
-    vector<int>nextgreater(vector<int>v){
-        stack<int>st;
-        int n=v.size();
-        vector<int>ans(n,-1);
-        for(int i=n-1;i>=0;i--){
-            while(st.size()>0){
-                if(st.top()<=v[i]){
-                    st.pop();
-                }else{
-                    ans[i]=st.top();
-                    break;
-                }
-            }
-            st.push(v[i]);
-        }
-        return ans;
-    }
+    // vector<int>nextgreater(vector<int>v){
+    //     stack<int>st;
+    //     int n=v.size();
+    //     vector<int>ans(n,-1);
+    //     for(int i=n-1;i>=0;i--){
+    //         while(st.size()>0){
+    //             if(st.top()<=v[i]){
+    //                 st.pop();
+    //             }else{
+    //                 ans[i]=st.top();
+    //                 break;
+    //             }
+    //         }
+    //         st.push(v[i]);
+    //     }
+    //     return ans;
+    // }
+
+    // vector<int> nextGreaterElements(vector<int>& nums) {
+    //     vector<int>nums2;
+    //     for(auto i:nums){
+    //         nums2.push_back(i);
+    //     }
+    //     for(auto i:nums){
+    //         nums2.push_back(i);
+    //     }
+    //     vector<int>ans=nextgreater(nums2);
+    //     int n=nums.size();
+    //     vector<int>f_ans(ans.begin(),ans.begin()+n);
+    //     return f_ans;
+    // }
 
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int>nums2;
-        for(auto i:nums){
-            nums2.push_back(i);
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> st;
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int idx = i % n;
+
+            while (!st.empty() && st.top() <= nums[idx])
+                st.pop();
+
+            if (i < n && !st.empty())
+                ans[idx] = st.top();
+
+            st.push(nums[idx]);
         }
-        for(auto i:nums){
-            nums2.push_back(i);
-        }
-        vector<int>ans=nextgreater(nums2);
-        int n=nums.size();
-        vector<int>f_ans(ans.begin(),ans.begin()+n);
-        return f_ans;
+
+        return ans;
     }
 };
