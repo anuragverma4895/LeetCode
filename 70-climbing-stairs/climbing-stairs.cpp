aef407da44a0f09f07b08e0157c5c98cf7867777
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-04-13 22:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.08%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
+    // //  memorization
+    // int recur(int n,vector<int>&dp){
+    //     if(n==0 or n==1) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=recur(n-1,dp)+recur(n-2,dp);
+    // }
+    // int climbStairs(int n) {
 
-    int recur(int n,vector<int>&dp){
-        if(n==0 or n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=recur(n-1,dp)+recur(n-2,dp);
-    }
+    //     vector<int>dp(n+1,-1);
+    //     return recur(n,dp);
+
+    // }
+
+
     int climbStairs(int n) {
 
         vector<int>dp(n+1,-1);
-        return recur(n,dp);
-        // if(n<=2) return n;
-        // int a=1,b=2,result;
-        // for(int i=3;i<=n;i++){
-        //     result=a+b;
-        //     a=b;
-        //     b=result;
-        // }
-        // return b;
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
 
     }
 };
