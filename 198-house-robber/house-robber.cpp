086a0b16fafc44fac58090f504708720366ab33b
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-04-15 00:24:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+
     // // memoization start from starting
     // int recur(int idx,vector<int>&nums,vector<int>&dp){
     //     if(idx>=nums.size()) return 0;
@@ -16,20 +17,37 @@ public:
     //     return recur(0,nums,dp);
     // }
 
-    // memoization start from last
-    int recur(int n,vector<int>&nums,vector<int>&dp){
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int take=nums[n]+recur(n-2,nums,dp);
-        int nottake=0+recur(n-1,nums,dp);
-        return dp[n]=max(take,nottake);
-    }
+
+
+    // // memoization start from last
+    // int recur(int n,vector<int>&nums,vector<int>&dp){
+    //     if(n<0) return 0;
+    //     if(dp[n]!=-1) return dp[n];
+    //     int take=nums[n]+recur(n-2,nums,dp);
+    //     int nottake=0+recur(n-1,nums,dp);
+    //     return dp[n]=max(take,nottake);
+    // }
+    // int rob(vector<int>& nums){
+    //     int n=nums.size();
+    //     vector<int>dp(n+1,-1);
+    //     return recur(n-1,nums,dp);
+    // }
+
+    // tabulation
     int rob(vector<int>& nums){
         int n=nums.size();
         vector<int>dp(n+1,-1);
-        return recur(n-1,nums,dp);
-    }
+        for(int i=0;i<n;i++){
+            int take=nums[i];
+            if(i-2>=0)
+                take+=dp[i-2];
+            int nottake=0;
+            if(i-1>=0) nottake+=dp[i-1];
+            dp[i]=max(take,nottake);
 
+        }
+        return dp[n-1];
+    }
 
 
 };
