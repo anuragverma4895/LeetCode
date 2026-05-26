dbf0e2b813cadcdc1adfa1f98db17b8f921c1318
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-05-26 22:43:03 +0530

    Time: 72 ms (53.87%) | Memory: 24.4 MB (69.34%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+
+        vector<int> state(26, -1);
+
+        for(char ch : word) {
+
+            int idx = tolower(ch) - 'a';
+
+            if(islower(ch)) {
+
+                if(state[idx] == -1)
+                    state[idx] = 1;
+
+                else if(state[idx] == 0)
+                    state[idx] = 2;
+
+                else if(state[idx] == 3)
+                    state[idx] = 2;
+            }
+            else {
+
+                if(state[idx] == 1)
+                    state[idx] = 0;
+
+                else if(state[idx] == -1)
+                    state[idx] = 3;
+            }
+        }
+
+        int ans = 0;
+
+        for(int x : state) {
+            if(x == 0)
+                ans++;
+        }
+
+        return ans;
+    }
+};
