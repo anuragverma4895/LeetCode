d2d26d9cde751e23e8d8e9974461a46486cb7108
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-05-31 23:30:47 +0530

    Time: 37 ms (31.97%) | Memory: 106.7 MB (81.61%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+
+        long long m = mass;
+
+        for (int x : asteroids) {
+            if (m < x) return false;
+            m += x;
+        }
+
+        return true;
+    }
+    
+};
