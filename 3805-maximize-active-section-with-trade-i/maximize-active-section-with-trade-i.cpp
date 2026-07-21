e7fe92a84e9908a594ea3d409a01910f7d0be351
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-07-21 21:29:32 +0530

    Time: 42 ms (89.19%) | Memory: 55 MB (58.56%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n = s.size();
+        int ones = 0;
+        int pre = INT_MIN;
+        int mx = 0;
+
+        for (int i = 0; i < n; ) {
+            int j = i;
+            while (j < n && s[j] == s[i]) j++;
+
+            int len = j - i;
+
+            if (s[i] == '1') {
+                ones += len;
+            } else {
+                mx = max(mx, pre + len);
+                pre = len;
+            }
+
+            i = j;
+        }
+
+        return ones + mx;
+    }
+};
