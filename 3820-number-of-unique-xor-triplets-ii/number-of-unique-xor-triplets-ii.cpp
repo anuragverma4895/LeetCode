28ab515a630f73b40ecada825edccc7ae7968a9d
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-07-24 22:30:10 +0530

    Time: 2051 ms (37.64%) | Memory: 98.1 MB (47.75%) - LeetSync

---
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        unordered_set<int> pairXor;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                pairXor.insert(nums[i] ^ nums[j]);
+            }
+        }
+
+        unordered_set<int> ans;
+
+        for (int x : pairXor) {
+            for (int v : nums) {
+                ans.insert(x ^ v);
+            }
+        }
+
+        return ans.size();
+    }
+};
