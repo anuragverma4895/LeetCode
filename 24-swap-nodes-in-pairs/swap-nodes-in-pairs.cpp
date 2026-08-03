7c8ffad01575f5bb8668a732271f9a441dd2d3a4
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-08-03 23:04:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (20.72%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -11,10 +11,28 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
+
+        // using recursion
         if(head==NULL || head->next==NULL) return head;
         ListNode* temp=head->next;
         head->next=swapPairs(temp->next);
         temp->next=head;
         return temp;
+
+
+        // by iterrative method
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* prev=dummy;
+        while(prev->next and prev->next->next){
+            ListNode* first=prev->next;
+            ListNode* second=first->next;
+
+            first->next=second->next;
+            second->next=first;
+            prev->next=second;
+            prev=first;
+        }
+        return dummy->next;
     }
 };
