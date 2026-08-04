06d989ca94542938791918cc9433b5fe44c761a4
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-08-04 21:44:49 +0530

    Time: 4 ms (35.49%) | Memory: 32.2 MB (75.54%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -5,9 +5,6 @@ public:
         sort(nums.begin(),nums.end());
         int a=nums[0];
         int n=nums[n-1];
-        int i=1;
-        
-        // a=nums[i-1]+1
         for(int i=1;i<nums.size();){
             if(a+1!=nums[i]){
                 ans.push_back(a+1);
