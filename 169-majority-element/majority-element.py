edbdedc7da180dcbfe07f338176040eae882f24e
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-08-12 23:05:42 +0530

    Time: 48 ms (6.59%) | Memory: 21.4 MB (18.49%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,17 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        ans=1
-        count=1
-        val=0
-        if len(nums)==1:
-            return nums[0]
-        for i in range(1,len(nums)):
-            if nums[i-1]==nums[i]:
-                count+=1
-                if count>ans:
-                    val=nums[i]
-                ans=max(ans,count)
-            else:
-                count=1
-        return val
+
+        # nums.sort()
+        # ans=1
+        # count=1
+        # val=0
+        # if len(nums)==1:
+        #     return nums[0]
+        # for i in range(1,len(nums)):
+        #     if nums[i-1]==nums[i]:
+        #         count+=1
+        #         if count>ans:
+        #             val=nums[i]
+        #         ans=max(ans,count)
+        #     else:
+        #         count=1
+        # return val
+
+        # other method
+        n=len(nums)
+        freq={}
+        for x in nums:
+            freq[x]=freq.get(x,0)+1
+            print(x,end=" ")
+        ans=0
+        maxval=0
+        for x in freq:
+            if freq[x] > maxval:
+                maxval = freq[x]
+                ans = x
+        return ans
