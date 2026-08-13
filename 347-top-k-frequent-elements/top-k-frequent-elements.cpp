5ab72ecec9513b3414a5cc5113f9787d7e19e3f1
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-08-14 02:20:59 +0530

    Time: 9 ms (8.27%) | Memory: 40 MB (7.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,19 +1,63 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> freq;
-        for(int x : nums){
+        // // brute form O(n^2)
+        // int n=nums.size();
+        // vector<int>ans;
+        // unordered_map<int,int>mp;
+        // for(auto x:nums){
+        //     mp[x]++;
+        // }
+        // while(k--){
+        //     int a=0;
+        //     int mx=INT_MIN;
+        //     for(auto x:mp){
+        //         if(a<x.second){
+        //             mx=x.first;
+        //             a=x.second;
+        //         }
+        //     }
+        //     ans.push_back(mx);
+        //     mp.erase(mx);
+        // }
+        // return ans;
+
+
+        // // optimal O(nlogn)
+        // unordered_map<int,int> freq;
+        // for(int x : nums){
+        //     freq[x]++;
+        // }
+        // priority_queue<pair<int,int>> pq; 
+        // for(auto &p : freq){
+        //     pq.push({p.second, p.first});
+        // }
+        // vector<int> ans;
+        // while(k--){
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // return ans;
+
+        // Optimal Tc= O(n),  Sc=O(n)
+        vector<int>ans;
+        vector<int>freq(200001,0);
+        int a=100000;
+        for(int x:nums){
+            x+=a;
             freq[x]++;
         }
-        priority_queue<pair<int,int>> pq; 
-        for(auto &p : freq){
-            pq.push({p.second, p.first});
+        vector<pair<int, int>>freq2;
+        for(int i=0;i<freq.size();i++){
+            if(freq[i]>0)
+                freq2.push_back({freq[i],i-a});
         }
-        vector<int> ans;
-        while(k--){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        sort(freq2.begin(),freq2.end(),greater<pair<int,int>>());
+        for(int i=0;i<k;i++){
+            ans.push_back(freq2[i].second);
         }
         return ans;
+
+
     }
 };
