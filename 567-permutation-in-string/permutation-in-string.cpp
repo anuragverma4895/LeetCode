4df874e8ff8a1c86577a7edfb498f952ad6d3fd7
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-08-16 01:12:59 +0530

    Time: 4 ms (52.42%) | Memory: 10.1 MB (31.51%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        // // TLE TC = O(n*n!*m)
+        // sort(s1.begin(),s1.end());
+        // do{
+        //     if(s2.find(s1)!=string::npos){
+        //         return true;
+        //     }
+        // }while(next_permutation(s1.begin(),s1.end()));
+        // return false;
+
+        // optimal code TC = O(n + m)
+        if (s1.size() > s2.size()) return false;
+
+        vector<int> freq1(26, 0), freq2(26, 0);
+
+        for (char c : s1)
+            freq1[c - 'a']++;
+
+        for (int i = 0; i < s1.size(); i++)
+            freq2[s2[i] - 'a']++;
+
+        if (freq1 == freq2)
+            return true;
+
+        for (int i = s1.size(); i < s2.size(); i++) {
+            freq2[s2[i] - 'a']++;
+            freq2[s2[i - s1.size()] - 'a']--;
+
+            if (freq1 == freq2)
+                return true;
+        }
+
+        return false;
+    }
+};
