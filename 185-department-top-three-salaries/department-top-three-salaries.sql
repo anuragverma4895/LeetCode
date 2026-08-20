a6f32c0a63cb01efbb5b3cf2bf87e4d52318c4ac
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-08-20 23:58:53 +0530

    Time: 1542 ms (13.21%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -6,9 +6,9 @@ SELECT
 FROM Employee e1
 JOIN Department d
     ON e1.departmentId = d.id
-WHERE 3 >= (
+WHERE 3 > (
     SELECT COUNT(DISTINCT e2.salary)
     FROM Employee e2
     WHERE e2.departmentId = e1.departmentId
-      AND e2.salary >= e1.salary
+      AND e2.salary > e1.salary
 );
