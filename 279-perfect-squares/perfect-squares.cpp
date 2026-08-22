239c6fe1a78f1f657593ff6c51552f55f97d94a2
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-08-22 22:47:18 +0530

    Time: 43 ms (69.51%) | Memory: 13 MB (85.28%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -3,7 +3,7 @@ public:
     int numSquares(int n) {
         vector<int>dp(n+1,INT_MAX);
         dp[0]=0;
-        for(int i=0;i<=n;i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j*j<=i;j++){
                 dp[i]=min(dp[i],dp[i-j*j]+1);
             }
