d61f2adab3fa64faec4751333eae0d55e78c677c
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-04 17:22:09 +0530

    Time: 4 ms (74.59%) | Memory: 48.2 MB (9.68%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
@@ -8,25 +8,52 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> nextLargerNodes(ListNode* head) {
+//         vector<int>ans;
+//         ListNode* temp=head;
+//         while(temp->next){
+//             ListNode* temp1=temp->next;
+//             while(temp1){
+//                 if(temp->val<temp1->val){
+//                     ans.push_back(temp1->val);
+//                     break;
+//                 }
+//                 temp1=temp1->next;
+//             }
+//             if(temp1==NULL)
+//                 ans.push_back(0);
+//             temp=temp->next;
+//         }
+//         ans.push_back(0);
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
-        vector<int>ans;
-        ListNode* temp=head;
-        while(temp->next){
-            ListNode* temp1=temp->next;
-            while(temp1){
-                if(temp->val<temp1->val){
-                    ans.push_back(temp1->val);
-                    break;
-                }
-                temp1=temp1->next;
+        vector<int> ans;
+        vector<pair<int,int>> st;
+
+        ListNode* temp = head;
+        int i = 0;
+
+        while(temp) {
+            while(!st.empty() && st.back().first < temp->val) {
+                ans[st.back().second] = temp->val;
+                st.pop_back();
             }
-            if(temp1==NULL)
-                ans.push_back(0);
-            temp=temp->next;
+
+            ans.push_back(0);
+            st.push_back({temp->val, i});
+
+            temp = temp->next;
+            i++;
         }
-        ans.push_back(0);
+
         return ans;
     }
 };
