a7153f061cf8d3293b434d1a99f221f309c32e3f
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-12 18:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.53%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -9,20 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void flatten(TreeNode* root) {
+//         if(root==NULL) return;
+//         flatten(root->left);
+//         flatten(root->right);
+//         TreeNode* p=root->right;
+//         root->right=root->left;
+//         root->left=NULL;
+
+//         TreeNode* curr=root;
+//         while(curr->right){
+//             curr=curr->right;
+//         }
+//         curr->right=p;
+//     }
+// };
+
+
 class Solution {
 public:
-    void flatten(TreeNode* root) {
+    void build(TreeNode* root, TreeNode*&prev){
         if(root==NULL) return;
-        flatten(root->left);
-        flatten(root->right);
-        TreeNode* p=root->right;
-        root->right=root->left;
+        build(root->right,prev);
+        build(root->left,prev);
+        root->right=prev;
         root->left=NULL;
-
-        TreeNode* curr=root;
-        while(curr->right){
-            curr=curr->right;
-        }
-        curr->right=p;
+        prev=root;
+    }
+    void flatten(TreeNode* root){
+        TreeNode* prev=NULL;
+        build(root,prev);
     }
 };
