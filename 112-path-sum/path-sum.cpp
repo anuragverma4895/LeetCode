b807264f2306e76d0f02b96d3b3301c91f8e0568
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-13 18:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (34.87%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -9,21 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     bool path(TreeNode* root, int target, int sum){
+//         if(root==NULL) return false;
+//         sum+=root->val;
+//         if(root->left==NULL && root->right==NULL){
+//             if(sum==target) return true;
+//         }
+//         if(path(root->left,target,sum))return true;;
+//         if(path(root->right,target,sum))return true;
+//         return false;
+//     }
+
+//     bool hasPathSum(TreeNode* root, int targetSum) {
+//         int sum=0;
+//         return path(root,targetSum,sum);
+//     }
+// };
+
+
+
 class Solution {
 public:
-    bool path(TreeNode* root, int target, int sum){
-        if(root==NULL) return false;
-        sum+=root->val;
-        if(root->left==NULL && root->right==NULL){
-            if(sum==target) return true;
-        }
-        if(path(root->left,target,sum))return true;;
-        if(path(root->right,target,sum))return true;
-        return false;
-    }
-
     bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum=0;
-        return path(root,targetSum,sum);
+        if(root==NULL) return false;
+        targetSum-=root->val;
+        if(root->left==NULL and root->right==NULL){
+            return targetSum==0;
+        }
+        return hasPathSum(root->left,targetSum) or hasPathSum(root->right,targetSum);
     }
 };
