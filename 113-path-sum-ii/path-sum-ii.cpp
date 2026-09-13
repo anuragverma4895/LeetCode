a43ddc1d08c873e588a24bf64bb86550643c3884
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-13 18:53:31 +0530

    Time: 19 ms (7.34%) | Memory: 40.8 MB (6.23%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -9,27 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void path(TreeNode* root, int target, vector<int>temp,int sum,vector<vector<int>>&ans){
+//         if(root==NULL) return;
+//         sum+=root->val;
+//         temp.push_back(root->val);
+//         if(root->left==NULL && root->right==NULL){
+//             if(sum==target){
+//                 ans.push_back(temp);
+//             }
+//         }
+//         path(root->left,target,temp,sum,ans);
+//         path(root->right,target,temp,sum,ans);
+//     }    
+
+//     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+//         vector<vector<int>>ans;
+//         vector<int>temp;
+//         int sum=0;
+//         path(root,targetSum,temp,sum,ans);
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
 
-    void path(TreeNode* root, int target, vector<int>temp,int sum,vector<vector<int>>&ans){
+    void path(TreeNode* root, int target, vector<int>temp,vector<vector<int>>&ans){
         if(root==NULL) return;
-        sum+=root->val;
+        target-=root->val;
         temp.push_back(root->val);
         if(root->left==NULL && root->right==NULL){
-            if(sum==target){
+            if(target==0){
                 ans.push_back(temp);
             }
         }
-        path(root->left,target,temp,sum,ans);
-        path(root->right,target,temp,sum,ans);
+        path(root->left,target,temp,ans);
+        path(root->right,target,temp,ans);
     }    
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>>ans;
         vector<int>temp;
-        int sum=0;
-        path(root,targetSum,temp,sum,ans);
+        path(root,targetSum,temp,ans);
         return ans;
     }
 };
