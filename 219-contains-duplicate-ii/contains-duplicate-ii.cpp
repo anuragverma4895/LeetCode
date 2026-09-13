7a674684e164f3007587f246e86d4cfb3a2e9f13
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-13 22:49:10 +0530

    Time: 109 ms (56.61%) | Memory: 142 MB (30.74%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // // TLE
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(nums[i]==nums[j] and abs(i-j)<=k){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i]) != mp.end()){
+                if(i-mp[nums[i]] <= k)
+                    return true;
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
