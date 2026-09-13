81d996c8ff4ba5afc7909cffc5e962d837573ad8
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-13 19:31:31 +0530

    Time: 8 ms (51.69%) | Memory: 18.9 MB (89.11%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -9,20 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int totalpath(TreeNode* root, long long target){
+//         if(root==NULL) return 0;
+//         long long count=0;
+//         if(root->val==target) count++;
+//         count+=totalpath(root->left,target-root->val);
+//         count+=totalpath(root->right,target-root->val);
+//         return count;
+//     }
+
+//     int pathSum(TreeNode* root, int targetSum) {
+//         if(root==NULL) return 0;
+//         return totalpath(root,targetSum)+pathSum(root->left,targetSum)+pathSum(root->right,targetSum);
+//     }
+// };
+
+
+
 class Solution {
 public:
-
-    int totalpath(TreeNode* root, long long target){
-        if(root==NULL) return 0;
-        long long count=0;
-        if(root->val==target) count++;
-        count+=totalpath(root->left,target-root->val);
-        count+=totalpath(root->right,target-root->val);
-        return count;
+    void dfs(TreeNode* root, long long targetSum,long long &ans){
+        if(root==NULL) return;
+        targetSum-=root->val;
+        if(targetSum==0){
+            ans++;
+        }
+        dfs(root->left,targetSum,ans);
+        dfs(root->right,targetSum,ans);
     }
-
+    long long ans=0;
     int pathSum(TreeNode* root, int targetSum) {
         if(root==NULL) return 0;
-        return totalpath(root,targetSum)+pathSum(root->left,targetSum)+pathSum(root->right,targetSum);
+        dfs(root,targetSum,ans);
+        pathSum(root->left,targetSum);
+        pathSum(root->right,targetSum);
+        return int(ans);
     }
 };
