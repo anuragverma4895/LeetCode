9a1f575faba5665463ddefbf3d86d3a2f9459479
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-14 23:36:14 +0530

    Time: 124 ms (26.63%) | Memory: 142 MB (28.52%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -12,16 +12,13 @@ public:
         // return false;
 
         unordered_map<int,int> mp;
-
         for(int i=0;i<nums.size();i++){
             if(mp.find(nums[i]) != mp.end()){
                 if(i-mp[nums[i]] <= k)
                     return true;
             }
-
             mp[nums[i]] = i;
         }
-
         return false;
     }
 };
