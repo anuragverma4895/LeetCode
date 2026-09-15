98609569e531a067895175e47d41bc6c12eaf89d
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-15 23:47:25 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (49.79%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,15 +1,39 @@
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& a, vector<int>& b) {
+//         unordered_map<int,int>freq;
+//         for(int x:a){
+//             freq[x]++;
+//         } 
+//         vector<int>ans;
+//         for(int x:b){
+//             if(freq[x]>0){
+//                 freq[x]--;
+//                 ans.push_back(x);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& a, vector<int>& b) {
-        unordered_map<int,int>freq;
-        for(int x:a){
-            freq[x]++;
-        } 
+        unordered_map<int,int>mp;
         vector<int>ans;
-        for(int x:b){
-            if(freq[x]>0){
-                freq[x]--;
-                ans.push_back(x);
+        for(int i=0;i<a.size();i++){
+            mp[a[i]]++;
+        }
+
+        for(int i=0;i<b.size();i++){
+            if(mp.find(b[i])!=mp.end()){
+                mp[b[i]]--;
+                if(mp[b[i]]==0){
+                    mp.erase(b[i]);
+                }
+                ans.push_back(b[i]);
             }
         }
         return ans;
