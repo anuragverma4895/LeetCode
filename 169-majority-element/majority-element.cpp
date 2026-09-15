0aefd3b4152c9daffed2a058f21c9da0d21b9edf
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-16 00:07:35 +0530

    Time: 3 ms (28.76%) | Memory: 42.3 MB (15.38%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,39 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int winner=-1;
+//         int count=0;
+//         for(auto x:nums){
+//             if(count==0){
+//                 count=1;
+//                 winner=x;
+//             }else if(x==winner){
+//                 count++;
+//             }else{
+//                 count--;
+//             }
+//         }
+//         return winner;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int winner=-1;
-        int count=0;
-        for(auto x:nums){
-            if(count==0){
-                count=1;
-                winner=x;
-            }else if(x==winner){
-                count++;
-            }else{
-                count--;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int ans=INT_MIN;
+        int temp=INT_MIN;
+        for(auto it:mp){
+            if(it.second>temp){
+                temp=it.second;
+                ans=it.first;
             }
         }
-        return winner;
+        return ans;
     }
 };
