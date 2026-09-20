111653faa87ca69919b0880c4138f7e0325aa131
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-20 11:34:51 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (98.24%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,10 +1,24 @@
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         // int n=s.size();
+//         // for(int i=0;i<n/2;i++){
+//         //     swap(s[i],s[n-i-1]);
+//         // }
+//         reverse(s.begin(),s.end());
+//     }
+// };
+
+
 class Solution {
 public:
+    void f(vector<char>&s,int i,int sz,int n){
+        if(i>=sz) return;
+        swap(s[i],s[n-i-1]);
+        f(s,i+1,sz,n);
+    }
     void reverseString(vector<char>& s) {
-        // int n=s.size();
-        // for(int i=0;i<n/2;i++){
-        //     swap(s[i],s[n-i-1]);
-        // }
-        reverse(s.begin(),s.end());
+        f(s,0,s.size()/2,s.size());
+        
     }
 };
