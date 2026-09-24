dfb6fd405703e964ba51b67b285dfb3708917832
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-24 22:06:16 +0530

    Time: 3 ms (31.61%) | Memory: 15 MB (15.87%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -18,24 +18,45 @@
 
 
 
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& a, vector<int>& b) {
+//         unordered_map<int,int>mp;
+//         vector<int>ans;
+//         for(int i=0;i<a.size();i++){
+//             mp[a[i]]++;
+//         }
+
+//         for(int i=0;i<b.size();i++){
+//             if(mp.find(b[i])!=mp.end()){
+//                 mp[b[i]]--;
+//                 if(mp[b[i]]==0){
+//                     mp.erase(b[i]);
+//                 }
+//                 ans.push_back(b[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> intersect(vector<int>& a, vector<int>& b) {
-        unordered_map<int,int>mp;
-        vector<int>ans;
-        for(int i=0;i<a.size();i++){
-            mp[a[i]]++;
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+
+        for(int x : nums2) {
+            mp[x]++;
         }
 
-        for(int i=0;i<b.size();i++){
-            if(mp.find(b[i])!=mp.end()){
-                mp[b[i]]--;
-                if(mp[b[i]]==0){
-                    mp.erase(b[i]);
-                }
-                ans.push_back(b[i]);
+        for(int x : nums1) {
+            if(mp[x] > 0) {
+                ans.push_back(x);
+                mp[x]--;
             }
         }
+
         return ans;
     }
 };
