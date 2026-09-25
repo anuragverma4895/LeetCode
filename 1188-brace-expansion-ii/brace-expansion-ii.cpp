ed160d51449f80aeb0c78a73f4e4946df439ea8f
Anurag Verma <180851620+anuragverma4895@users.noreply.github.com>
2026-09-25 22:23:15 +0530

    Time: 11 ms (49.11%) | Memory: 17.5 MB (28.57%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    set<string> solve(string &s, int &i) {
+        set<string> res;
+        set<string> cur;
+        cur.insert("");
+
+        while (i < s.size() && s[i] != '}') {
+            if (s[i] == ',') {
+                for (auto x : cur) res.insert(x);
+                cur.clear();
+                cur.insert("");
+                i++;
+            }
+            else if (s[i] == '{') {
+                i++;
+                set<string> temp = solve(s, i);
+                i++;
+
+                set<string> next;
+                for (auto a : cur) {
+                    for (auto b : temp) {
+                        next.insert(a + b);
+                    }
+                }
+                cur = next;
+            }
+            else {
+                string x(1, s[i]);
+                set<string> next;
+
+                for (auto a : cur) {
+                    next.insert(a + x);
+                }
+
+                cur = next;
+                i++;
+            }
+        }
+
+        for (auto x : cur) res.insert(x);
+
+        return res;
+    }
+
+    vector<string> braceExpansionII(string expression) {
+        int i = 0;
+        set<string> ans = solve(expression, i);
+
+        return vector<string>(ans.begin(), ans.end());
+    }
+};
